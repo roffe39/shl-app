a347ba2a072573164b1407d239610412e07efe83
Roffe <dvdroffe@hotmail.com>
2020-03-16 13:53:11 +0100

    adde some more

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 server.applyMiddleware({ app });
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen({ port }, () => {
   console.log(
